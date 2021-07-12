ba6a399f92fe4cacabeae968695db6982505542f
mkabilan19 <m.kabilan@iitg.ac.in>
2021-07-12 11:43:35 +0530

    finally added all changes .. porject finished

--- Front End projects/script.js
+++ Front End projects/script.js
@@ -5,12 +5,40 @@ $(document).ready(function(){
     }else{
       $('.navbar').removeClass("sticky");
     }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+
   });
+  // slide up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+  });
+
+ // typing animation
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "IITian", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "IITian", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
   // toggle menu/navbar script
   $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
   });
+
+
   // owl carousel script
    $('.carousel').owlCarousel({
       margin: 20,
